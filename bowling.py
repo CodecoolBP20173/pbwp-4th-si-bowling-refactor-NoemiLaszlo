2b2463f5adb3af6e248a739a31e55cbc858e91d2
NoemiLaszlo <mynadesign@gmail.com>
2017-11-30 15:14:56 +0100

    better names, minus 1 elif

--- bowling.py
+++ bowling.py
@@ -1,43 +1,45 @@
 def score(game):
+    # docstring, kell mindhárom paraméter?
     result = 0
     frame = 1
     in_first_half = True
-    for i in range(len(game)):
-        if game[i] == '/':
-            result += 10 - last
+    for item in range(len(game)):  # i helyett más változónév
+        if game[item] == '/':
+            result += 10 - last  # last???
         else:
-            result += get_value(game[i])
+            result += get_value(game[item])
         # if not in_first_half:
             # frame += 1
-        if frame < 10  and get_value(game[i]) == 10:
-            if game[i] == '/':
-                result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
-                if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
+        if frame < 10 and get_value(game[item]) == 10:
+            if game[item] == '/':
+                result += get_value(game[item+1])
+            elif game[item] == 'X' or game[item] == 'x':
+                result += get_value(game[item+1])
+                if game[item+2] == '/':
+                    result += 10 - get_value(game[item+1])
                 else:
-                    result += get_value(game[i+2])
-        last = get_value(game[i])
+                    result += get_value(game[item+2])
+        last = get_value(game[item])
         if not in_first_half:
             frame += 1
-        if in_first_half == True:
+        if in_first_half is True:
             in_first_half = False
         else:
             in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
+        if game[item] == 'X' or game[item] == 'x':
             in_first_half = True
             frame += 1
     return result
 
+
 def get_value(char):
+    # docstring + if in one line
     if char == '1' or char == '2' or char == '3' or \
        char == '4' or char == '5' or char == '6' or \
        char == '7' or char == '8' or char == '9':
         return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
+    # .upper vagy ilyesmi egy ellenőrzés
+    elif char == 'X' or char == 'x' or char == '/':
         return 10
     elif char == '-':
         return 0
