f00a222a73c9392c151ad4c6c29589ef6afbce45
NoemiLaszlo <mynadesign@gmail.com>
2017-12-01 14:18:54 +0100

    komments, docstrings and some abridgments

--- bowling.py
+++ bowling.py
@@ -1,45 +1,44 @@
 def score(game):
-    # docstring, kell mindhárom paraméter?
-    result = 0
+    '''This function is "plays" the game frame by frame and returns the final score '''
+    final_score = 0
     frame = 1
-    in_first_half = True
-    for item in range(len(game)):  # i helyett más változónév
-        if game[item] == '/':
-            result += 10 - last  # last???
+    has_frame_more_rolls = True
+    for roll in range(len(game)):
+        # if the player rolls spare
+        if game[roll] == '/':
+            final_score += 10 - get_value(game[roll-1]) if roll > 0 else 0
         else:
-            result += get_value(game[item])
-        # if not in_first_half:
-            # frame += 1
-        if frame < 10 and get_value(game[item]) == 10:
-            if game[item] == '/':
-                result += get_value(game[item+1])
-            elif game[item] == 'X' or game[item] == 'x':
-                result += get_value(game[item+1])
-                if game[item+2] == '/':
-                    result += 10 - get_value(game[item+1])
+            final_score += get_value(game[roll])
+        # counting points through the game
+        if frame < 10 and get_value(game[roll]) == 10:
+            if game[roll] == '/':
+                final_score += get_value(game[roll+1])
+            elif game[roll].upper() == 'X':
+                final_score += get_value(game[roll+1])
+                if game[roll+2] == '/':
+                    final_score += 10 - get_value(game[roll+1])
                 else:
-                    result += get_value(game[item+2])
-        last = get_value(game[item])
-        if not in_first_half:
+                    final_score += get_value(game[roll+2])
+        # komment
+        if not has_frame_more_rolls:
             frame += 1
-        if in_first_half is True:
-            in_first_half = False
+        # komment
+        if has_frame_more_rolls is True:  # in_first_half = not in_first_half
+            has_frame_more_rolls = False
         else:
-            in_first_half = True
-        if game[item] == 'X' or game[item] == 'x':
-            in_first_half = True
+            has_frame_more_rolls = True
+        # komment
+        if game[roll].upper() == 'X':
+            has_frame_more_rolls = True
             frame += 1
-    return result
+    return final_score
 
 
 def get_value(char):
-    # docstring + if in one line
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+    '''This function returns the score of one roll'''
+    if char.isdigit() and len(char) == 1:
         return int(char)
-    # .upper vagy ilyesmi egy ellenőrzés
-    elif char == 'X' or char == 'x' or char == '/':
+    elif char.upper() == 'X' or char == '/':
         return 10
     elif char == '-':
         return 0
